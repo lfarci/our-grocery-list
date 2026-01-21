a0d07a9de5eee2d3a7d378ceb4c3dbffbf282c8e
Logan Farci <31938998+lfarci@users.noreply.github.com>
2026-01-21 09:25:41 +0100

    Enable Playwright parallelism in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  /* Allow parallel tests on CI. */
+  workers: process.env.CI ? 2 : undefined,
   
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['html'], ['github']] : 'html',
